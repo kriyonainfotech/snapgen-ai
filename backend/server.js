e10b44c13670b2bf15c05967ed8c9c0a63f4b6b8
kriyona infotech <kriyonainfotech@gmail.com>
2026-01-03 17:35:48 +0530

    save

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5174',
+    origin: ['http://localhost:5174'],
     credentials: true
 }));
 
