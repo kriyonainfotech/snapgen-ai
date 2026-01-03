848082a98ff72bd2294f82a4c736b1abb85fa818
kriyona infotech <kriyonainfotech@gmail.com>
2026-01-03 17:40:36 +0530

    save

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ['http://localhost:5174'],
+    origin: ['http://localhost:5174', "https://snapgenai.vercel.app"],
     credentials: true
 }));
 
